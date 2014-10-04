6320b942b7aad408c79cd941842fe8f36dbd669e
i0epi1 <i0epi1@users.noreply.github.com>
2014-10-04 08:02:38 +0530

    Create sample.cpp
    contains included header files and the ability to use file input or standard input (un/comment line 1)

---
+++ sample.cpp
@@ -0,0 +1,27 @@
+//#define LOCAL_TEST
+
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <complex>
+#include <cstring>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <cmath>
+#include <map>
+#include <set>
+using namespace std;
+
+int main()
+{
+	#ifdef LOCAL_TEST
+		freopen("in.txt", "r", stdin);
+		freopen("out.txt", "w", stdout);
+	#endif
+	ios :: sync_with_stdio(false);
+	cout << fixed << setprecision(16);
+	/* Main Code Here */
+	return 0;
+}
